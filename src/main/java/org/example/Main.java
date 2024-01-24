909381387fd1f9ed294c43e3c470f88e72547977
wbryon <tamirbudaev.habibula@gmail.com>
2024-01-24 19:10:41 +0300

    feat: added Java version

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 3, 4, 5, 1, 5, 4);
+
+        System.out.println(countNumberOfElements(list).entrySet());
+    }
+
+    public static Map<Integer,Integer> countNumberOfElements(List<Integer> list) {
+        Map<Integer, Integer> map = new HashMap<>();
+        list.forEach(integer -> map.put(integer, map.getOrDefault(integer, 0) + 1));
+        return map;
+    }
+}
